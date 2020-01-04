d8a22c9cef9f012b18d9d73a8a4d6028c5638b42
Anna Volkova-Kravchenko <anvolkravt@ya.ru>
2020-01-04 14:06:22 +0300

    add AudioController class with methods for bg music

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', ready());
-} else {
-  ready();
+class AudioController {
+  constructor() {
+    this.bgMusic = new Audio('Assets/Audio/creepy.mp3');
+    this.flipSound = new Audio('Assets/Audio/flip.wav');
+    this.matchSound = new Audio('Assets/Audio/match.wav');
+    this.victorySound = new Audio('Assets/Audio/victory.wav');
+    this.gameOverSound = new Audio('Assets/Audio/gameover.wav');
+    this.bgMusic.volume = 0.5;
+    this.bgMusic.loop = true;
+  }
+
+  startMusic() {
+    this.bgMusic.play();
+  }
+
+  stopMusic() {
+    this.bgMusic.pause();
+    this.bgMusic.currentTime = 0;
+  }
 }
 
 function ready() {
@@ -12,6 +27,8 @@ function ready() {
     overlay.addEventListener('click', () => {
       overlay.classList.remove('visible');
       // game.startGame();
+      let audioController = new AudioController();
+      audioController.startMusic();
     });
   });
 
@@ -20,4 +37,10 @@ function ready() {
       // game.flipCard(card);
     });
   });
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', ready());
+} else {
+  ready();
 }
