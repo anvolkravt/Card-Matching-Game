b366fc6a0b92cdabcb858af021c2db22bec3382f
Anna Volkova-Kravchenko <anvolkravt@ya.ru>
2020-01-04 15:43:46 +0300

    add victory and gam over scenarios

--- script.js
+++ script.js
@@ -100,6 +100,8 @@ class MixOrMatch {
     if (this.getCardType(card) === this.getCardType(this.cardToCheck))
       this.cardMatch(card, this.cardToCheck);
     else this.cardMisMatch(card, this.cardToCheck);
+
+    this.cardToCheck = null;
   }
 
   cardMatch(card1, card2) {
@@ -108,10 +110,17 @@ class MixOrMatch {
     card1.classList.add('matched');
     card2.classList.add('matched');
     this.audioController.match();
-    if (this.matchedCards.length === this.cardsArray) this.victory();
+    if (this.matchedCards.length === this.cardsArray.length) this.victory();
   }
 
-  cardMisMatch(card) {}
+  cardMisMatch(card1, card2) {
+    this.busy = true;
+    setTimeout(() => {
+      card1.classList.remove('visible');
+      card2.classList.remove('visible');
+      this.busy = false;
+    }, 1000);
+  }
 
   getCardType(card) {
     return card.getElementsByClassName('card-value')[0].src;
@@ -126,12 +135,11 @@ class MixOrMatch {
   }
 
   canFlipCard(card) {
-    return true;
-    // return (
-    //   !this.busy &&
-    //   !this.matchedCards.includes(card) &&
-    //   card !== this.cardToCheck
-    // );
+    return (
+      !this.busy &&
+      !this.matchedCards.includes(card) &&
+      card !== this.cardToCheck
+    );
   }
 
   gameOver() {
